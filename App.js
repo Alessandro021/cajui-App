ffb7b3f907535c5750b23f02becb8799a14d580f
Alessandro021 <alessandrodamata6@gmail.com>
2022-06-04 17:58:04 -0300

    usando o pacote webView para mostrar o site do cajui na tela do celular, criei uma logo para o app e criei umasplash para aparecer na hora de inicializar o aplicativo

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+
+  const url = 'https://cajui.ifnmg.edu.br/cajui/login';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{ uri: url}}
+      style={{ marginTop: 40}}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
